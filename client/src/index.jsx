4096a813bd6110e01ed731c97e5e7e750f989356
borisowsky <yuzirock@gmail.com>
2018-02-17 18:23:45 +0200

    Refactor

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 
 import App from './App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-const store = createStoreWithMiddleware(reducers, composeWithDevTools());
+const store = createStore(reducers, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
