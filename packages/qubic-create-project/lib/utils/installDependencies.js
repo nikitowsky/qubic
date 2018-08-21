9d90a86d72102cc7a53eef83ea8a8f0a47c573a9
borisowsky <yuzirock@gmail.com>
2018-08-21 22:51:37 +0200

    v2.0.0-beta.9

--- packages/qubic-create-project/lib/utils/installDependencies.js
+++ packages/qubic-create-project/lib/utils/installDependencies.js
@@ -29,7 +29,7 @@ const installDependencies = (directory) => {
   const packageManager = getPackageManager();
 
   const packagesToInstall = [
-    '@qubic/builder@2.0.0-beta.8',
+    '@qubic/builder@2.0.0-beta.9',
     'react',
     'react-dom',
     'react-hot-loader',
